b1e53bd7a28c961b999006c1ff2a8fe33d7cc192
Nyusuka <56803555+Nyusuka@users.noreply.github.com>
2019-10-26 19:34:21 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,10 +1,11 @@
 const Discord = require('discord.js')
 const bot = new Discord.Client()
 const token = process.env.token;
+const prefix = '&'
 
 client.login(token);
 
 bot.on("message", async message => {
 
 
-};
+});
