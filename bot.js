512f4b10d4838c43a5fbd6b595da48d23d0d19f1
Nyusuka <56803555+Nyusuka@users.noreply.github.com>
2019-12-22 17:36:30 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,4 +1,4 @@
-//Pas touche aux premières constantes. Sinon, ça va moins bien marcher.
+/Pas touche aux premières constantes. Sinon, ça va moins bien marcher.
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 const token = process.env.token;
@@ -27,6 +27,36 @@ bot.on("message", async message =>{
         }};
 });
 
+//Boucle expérimentale pour le statut du bot :
+bot.on('messageReactionAdd',async (reaction, user, member) =>{
+    //Constantes pour l'ID de la propriétaire et des contributeurs du bot. :
+        const ownerID = "382500192907165717"; //Nyusuka ; propriétaire du bot.
+        const contributorID_01 = "246395977450258432"; //Arkaxii ; testeur et maître du codage.
+    
+    //Autres constantes pour le bon fonctionnement des commandes. :
+        const prefix = '&';
+            if(message.content.indexOf(prefix) !== 0) return;
+        const args = message.content.slice(prefix.length).trim().split(/ +/g); 
+        const command = args.shift().toLowerCase();
+    
+        if(command === "aktivität"){
+            const aktivität = args.join(" ");
+            if(message.author.id !== ownerID && message.author.id !== contributorID_01) return message.channel.send("Désolée, mais cette commande n'est utilisable qu'à des fins expérimentales par les personnes qui contribuent à mon développement. Si vous souhaitez l'utiliser et apporter votre maigre soutien, veuillez ~~aller vous faire foutre~~ postuler via la commande `&suggest` ou vous adresser directement à ma créatrice.");
+            if(!aktivität) return message.reply("AKTIVITÄT ! SCHNELL !");
+            const aktivität_log_display = member.guild.channels.find("name", "risilogs")
+            bot.user.setActivity(aktivität);
+        message.delete().catch(O_o=>{});
+        console.log(`Le statut du bot à été changé pour "Joue à ${aktivität}" par ${message.author.username}#${message.author.discriminator} (ID : ${message.author.id}).`);
+        var aktivität_log = new Discord.RichEmbed()
+            .setColor('RANDOM')
+            .setTitle("Logs :")
+            .setThumbnail(message.author.avatarURL)
+            .setDescription(`Le statut du bot à été changé pour "**Joue à** ${aktivität}" par ${message.author.username}#${message.author.discriminator} (ID : ${message.author.id}).`)
+            .setTimestamp()
+        return aktivität_log_display.send(aktivität_log);
+    };
+});
+
 //Boucle contenant les commandes :
 bot.on("message", async message => {
     //Constantes pour l'ID de la propriétaire et des contributeurs du bot. :
@@ -42,7 +72,7 @@ bot.on("message", async message => {
 //Commandes réservées à Nyusuka et aux contributeurs du développement du bot :
 
     //Commande pour changer le statut du bot. [&aktivität] :
-        if(command === "aktivität"){
+/*        if(command === "aktivität"){
             const aktivität = args.join(" ");
             if(message.author.id !== ownerID && message.author.id !== contributorID_01) return message.channel.send("Désolée, mais cette commande n'est utilisable qu'à des fins expérimentales par les personnes qui contribuent à mon développement. Si vous souhaitez l'utiliser et apporter votre maigre soutien, veuillez ~~aller vous faire foutre~~ postuler via la commande `&suggest` ou vous adresser directement à ma créatrice.");
             if(!aktivität) return message.reply("AKTIVITÄT ! SCHNELL !");
@@ -57,7 +87,7 @@ bot.on("message", async message => {
                 .setDescription(`Le statut du bot à été changé pour "**Joue à** ${aktivität}" par ${message.author.username}#${message.author.discriminator} (ID : ${message.author.id}).`)
                 .setTimestamp()
             return aktivität_log_display.send(aktivität_log);
-        };
+        };*/
 
 //Commandes expérimentales :
 
